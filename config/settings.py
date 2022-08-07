1f4d784e4da82bbc5f5a15efdcae053c079d4e63
Marcin Osiadacz <osiadacz.m@gmail.com>
2022-08-08 00:59:29 +0200

    configure db using dj_database_url for apply 12 factor app for db connection

--- config/settings.py
+++ config/settings.py
@@ -11,8 +11,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-import environ
 import os
+
+import environ
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -84,6 +87,11 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+DATABASE_URL = os.environ.get('DB_CONNECTION_STRING')
+db_config = dj_database_url.config(default=DATABASE_URL, conn_max_age=500, ssl_require=False)
+
+DATABASES['default'].update(db_config)
 
 
 # Password validation
